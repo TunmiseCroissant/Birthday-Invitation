2e229df3295089cde5b898a6d3dba348a512e5b0
TunmiseCroissant <tunmifolorunsho@gmail.com>
2025-11-22 17:44:57 -0600

    added music and link

---
+++ script.js
@@ -0,0 +1,10 @@
+  bgMusic = document.getElementById("bgMusic")
+  
+  function playMusic() {
+    bgMusic.play().catch(err => console.log("Autoplay blocked:", err));
+    document.removeEventListener("click", playMusic);
+    document.removeEventListener("keydown", playMusic);
+  }
+
+  document.addEventListener("click", playMusic);
+  document.addEventListener("keydown", playMusic);
